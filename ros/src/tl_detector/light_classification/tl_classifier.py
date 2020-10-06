88a36eebebbfc8211ad696704c5c785a9729de52
stevenjnovotny <steven.j.novotny@gmail.com>
2020-10-06 11:58:45 -0600

    added light classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -30,10 +30,10 @@ class TLClassifier(object):
         """Loads a frozen inference graph"""
         graph = tf.Graph()
         with graph.as_default():
-            # od_graph_def = tf.GraphDef()
-            od_graph_def = tf.compat.v1.GraphDef()
-            #with tf.gfile.GFile(graph_file, 'rb') as fid:
-            with tf.io.gfile.GFile(graph_file, 'rb') as fid:
+            od_graph_def = tf.GraphDef()
+            #od_graph_def = tf.compat.v1.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+            #with tf.io.gfile.GFile(graph_file, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
