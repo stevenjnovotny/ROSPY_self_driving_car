87a4079b47512b92f6adcba398ace79c0325e391
stevenjnovotny <steven.j.novotny@gmail.com>
2020-10-06 10:49:20 -0600

    added light classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,44 @@
 from styx_msgs.msg import TrafficLight
 
+import tensorflow as tf
+import numpy as np
+import time
+from scipy.stats import norm, mode
+
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+
+        GRAPH_FILE = 'frozen_inference_graph.pb'
+        
+        self.detection_graph = self.load_graph(GRAPH_FILE)
+
+        # `get_tensor_by_name` returns the Tensor with the associated name in the Graph.
+        self.image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+
+        # The classification of the object (integer id).
+        self.detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+
+    def load_graph(self, graph_file):
+        """Loads a frozen inference graph"""
+        graph = tf.Graph()
+        with graph.as_default():
+            # od_graph_def = tf.GraphDef()
+            od_graph_def = tf.compat.v1.GraphDef()
+            #with tf.gfile.GFile(graph_file, 'rb') as fid:
+            with tf.io.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        return graph
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +50,29 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+
+        image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
+
+        with tf.Session(graph=detection_graph) as sess:                
+            # Actual detection.
+            (boxes, scores, classes) = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes], 
+                                                feed_dict={self.image_tensor: image_np})
+
+            # Remove unnecessary dimensions
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes)
+
+
+            confidence_cutoff = 0.6
+            # Filter boxes with a confidence score less than `confidence_cutoff`
+            boxes, scores, classes = filter_boxes(confidence_cutoff, boxes, scores, classes)
+
+            options = [TrafficLight.GREEN, TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.UNKNOWN]
+
+            if classes != None and len(classes) != 0:
+                result = options[int(mode(classes)[0][0])-1]
+                return result
+
+
         return TrafficLight.UNKNOWN
